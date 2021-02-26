88e06aea67bae1c9db7a49b5eaedde4ef174c91b
hiroshi <s20840011@gmail.com>
2021-02-26 22:49:21 +0900

    second

--- invoicely/settings.py
+++ invoicely/settings.py
@@ -34,7 +34,7 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
